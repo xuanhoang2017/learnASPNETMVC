9801360226ff2eed1b8aad5af7aca88202e98e7b
vuxuanhoang2017 <hominhthu2017>
2019-03-07 15:05:15 +0700

    Update control

--- WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace WebApplication1
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("customHomepage", " Homepage/", new { Controller = "home", action = "Index" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
